5dda46a6554bd8738182e0c023c2d33de1127284
Komyint <myinthtunaung19@gmail.com>
2023-08-28 12:11:04 +0630

    added channel name

--- nodeapp_test-master/index.js
+++ nodeapp_test-master/index.js
@@ -2,20 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Computer Technology" }');
+    res.send('{ "response": "Hello Computer Technology" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World " }');
+    res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, Have A Nice Day!" }');
-});
-
-// Create a route to redirect to a YouTube link
-app.get('/youtube', (req, res) => {
-    // Redirect to the YouTube link
-    res.redirect('https://www.youtube.com/watch?v=YOUR_VIDEO_ID');
+    res.send('{ "response": " Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
